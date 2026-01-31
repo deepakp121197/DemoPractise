fc08e41ff18761ef25efa27236c0b4eec256dc5a
deepak <deepakrules.12@gmail.com>
2026-01-31 07:54:41 +0530

    US first commit

--- src/demoClass.java
+++ src/demoClass.java
@@ -3,6 +3,9 @@ public class demoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("US cloned");
+		System.out.println("Hello");
 
 	}
 
